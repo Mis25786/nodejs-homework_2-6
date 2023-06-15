1f641705196790a5dc8fbebfd0fb77aa320cc563
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-15 15:10:52 +0200

    create database end deploy to render.com

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Misha:PBYK1kIejqDDfgMP@cluster0.zwlkcdj.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
