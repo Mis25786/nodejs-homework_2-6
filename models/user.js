3998b5f0e4620d916de590e1ce9455068b96faf8
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-20 18:32:29 +0200

    add subscription end delete name in userSchema

--- models/user.js
+++ models/user.js
@@ -7,20 +7,21 @@ const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const userSchema = new Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       match: emailRegexp,
       unique: true,
-      required: true,
+      required: [true, "Email is required"],
     },
     password: {
       type: String,
       minlength: 6,
-      required: true,
+      required: [true, "Set password for user"],
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
     },
     token: {
       type: String,
