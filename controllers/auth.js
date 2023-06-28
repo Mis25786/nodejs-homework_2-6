ebe0d51c2cffbc64c1e65e57a2ef74fff1049442
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-28 12:01:30 +0200

    node hw-5 complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -92,10 +92,7 @@ const updateAvatar = async (req, res) => {
 
   const { file } = req;
   const img = await Jimp.read(file.path);
-  await img
-    .autocrop()
-    .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE)
-    .writeAsync(file.path);
+  await img.resize(250, 250).writeAsync(file.path);
 
   const filename = `${_id}_${originalname}`;
 
