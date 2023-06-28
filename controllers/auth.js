11ca31ecc8042dcad4222eae2b87cc7414b7bc96
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-28 15:35:45 +0200

    add resendVerifyEmail and router.post(/verify, validateBody(schemas.emailSchema), ctrl.resendVerifyEmail)

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,8 +10,7 @@ const { User } = require("../models/user");
 
 const { HttpError, ctrlWrapper, sendEmail } = require("../helpers");
 
-const { SECRET_KEY } = process.env;
-// const { SECRET_KEY, BASE_URL } = process.env;
+const { SECRET_KEY, BASE_URL } = process.env;
 
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
@@ -37,8 +36,7 @@ const register = async (req, res) => {
   const verifyEmail = {
     to: email,
     subject: "Verify email",
-    // html: `<a target="_blank" href="${BASE_URL}/auth/verify/${verificationToken}" >Click verify email</a>`,
-    html: `<a target="_blank" href="https://db-contacts-ki5i.onrender.com/auth/verify/${verificationToken}" >Click verify email</a>`,
+    html: `<a target="_blank" href="${BASE_URL}/auth/verify/${verificationToken}" >Click verify email</a>`,
   };
 
   await sendEmail(verifyEmail);
@@ -67,6 +65,32 @@ const verifyEmail = async (req, res) => {
 
   res.status(200).json({
     message: "Verification successful",
+  });
+};
+
+const resendVerifyEmail = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(400, "missing required field email");
+  }
+
+  if (user.verify) {
+    throw HttpError(400, "Verification has already been passed");
+  }
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/auth/verify/${user.verificationToken}" >Click verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.status(200).json({
+    message: "Verification email sent",
   });
 };
 
@@ -146,6 +170,7 @@ const updateAvatar = async (req, res) => {
 module.exports = {
   register: ctrlWrapper(register),
   verifyEmail: ctrlWrapper(verifyEmail),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
